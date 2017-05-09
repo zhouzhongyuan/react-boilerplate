85750b4dfd550aafc8915f8344f6bc302e3ea847
zhouzhongyuan <yawenzhongyuan@gmail.com>
2017-05-09 10:23:20 +0800

    Add HotModuleReplacement, fix webpackDevServer, fix svg loader

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,13 +1,28 @@
 import path from 'path';
+import webpack from 'webpack';
 const config = {
-    entry: path.resolve(__dirname, 'src/index.js'),
+    entry: [
+        'react-hot-loader/patch',
+        // 开启React代码的模块热替换（HMR）
+        'webpack-dev-server/client?http://localhost:8080',
+        // 为webnpack-dev-server的环境打包代码，然后连接到制定服务器域名与端口
+        'webpack/hot/only-dev-server',
+        //为热替换（HRM）打包好代码，only- 意味着只有成功更新运行代码才会执行HMR
+        path.resolve(__dirname, 'src/index.js')
+    ],
     output: {
         path: path.resolve(__dirname, 'build'),
-        publicPath: "assets",
         filename: 'bundle.js',
+        publicPath: "/",
     },
-    devtool: 'eval-source-map',
-    devServer: { inline: true },
+    devtool: 'cheap-eval-source-map',
+    devServer: {
+        compress: true,
+        contentBase: path.resolve(__dirname, 'public'),
+        port: 8080,
+        hot: true,
+        publicPath: '/',
+    },
     module: {
         loaders: [
             {
@@ -19,14 +34,19 @@ const config = {
                 loader: 'style-loader!css-loader',
             },
             {
-                test: /\.svg/,
-                use: {
-                    loader: 'svg-url-loader',
-                    options: {}
+                test: /\.svg$/,
+                loader: 'url-loader',
+                query: {
+                    limit: 1000,
+                    name: '[name]-[sha512:hash:base64:7].[ext]'
                 }
-            }
+            },
         ],
     },
-    watch: true,
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        // 开启全局的HMR
+        new webpack.NamedModulesPlugin(),
+    ]
 };
 module.exports = config;
